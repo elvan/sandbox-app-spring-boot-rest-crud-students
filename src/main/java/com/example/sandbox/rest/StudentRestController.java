e976efa53382f019204b47db5a8ada00fcceca3f
Elvan Hidayat <elvanhd@gmail.com>
2023-05-29 21:00:26 +0700

    get single student

--- src/main/java/com/example/sandbox/rest/StudentRestController.java
+++ src/main/java/com/example/sandbox/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,4 +38,13 @@ public class StudentRestController {
         return theStudents;
     }
 
+    // define endpoint or "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
+    }
 }
