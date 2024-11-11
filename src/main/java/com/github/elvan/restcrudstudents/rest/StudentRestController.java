736d77dd8134d5b01d56ce5f53ce5e1fa96c6148
Elvan Hidayat <elvanhd@gmail.com>
2024-11-11 23:44:16 +0700

    Optimize StudentRestController with data initialization
    - Add @PostConstruct to load student data on startup
    - Move student list to a class-level variable for reuse
    - Update /students endpoint to return preloaded student data

--- src/main/java/com/github/elvan/restcrudstudents/rest/StudentRestController.java
+++ src/main/java/com/github/elvan/restcrudstudents/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.github.elvan.restcrudstudents.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.annotation.PostConstruct;
+
 import com.github.elvan.restcrudstudents.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class StudentRestController {
 
-  // define endpoint for "/students" - return a list of students
+  private List<Student> theStudents;
 
-  @GetMapping("/students")
-  public List<Student> getStudents() {
+  // define @PostConstruct to load the student data ... only once!
 
-    List<Student> theStudents = new ArrayList<>();
+  @PostConstruct
+  public void loadData() {
+    theStudents = new ArrayList<>();
 
     theStudents.add(new Student("Poornima", "Patel"));
     theStudents.add(new Student("Mario", "Rossi"));
     theStudents.add(new Student("Mary", "Smith"));
+  }
 
+  // define endpoint for "/students" - return a list of students
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
     return theStudents;
   }
 
